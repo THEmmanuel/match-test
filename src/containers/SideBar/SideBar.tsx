d7cb859acb49196a3d03982dc6c4f1825eb2363a
Emmanuel <emmayodayo@gmail.com>
2023-03-14 18:52:53 +0100

    add icons

--- src/containers/SideBar/SideBar.tsx
+++ src/containers/SideBar/SideBar.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import style from './SideBar.module.css';
 
+// Icon imports
+import icon1 from '../../assets/sidebarIcon1.svg'
+import icon2 from '../../assets/sidebarIcon2.svg'
+import icon3 from '../../assets/sidebarIcon3.svg'
+import chartIcon from '../../assets/sidebarChartIcon.svg'
+import powerIcon from '../../assets/sidebarPowerIcon.svg'
+
+
 const SideBar = () => {
 	return (
 		<div className={style.SideBar}>
 			<div className={style.SideBarIconsWrapper}>
-				<span>Icon</span>
-				<span>Icon</span>
-				<span>Icon</span>
-				<span>Icon</span>
+				<button className={style.IconButton}>
+					<img src={icon1} alt="" />
+				</button>
+
+				<button className={style.IconButton}>
+					<img src={icon2} alt="" />
+				</button>
+
+				<button className={style.IconButton}>
+					<img src={icon3} alt="" />
+				</button>
+
+				<button className={style.IconButton}>
+					<img src={chartIcon} alt="" />
+				</button>
+
+				<button className={style.IconButton}>
+					<img src={powerIcon} alt="" />
+				</button>
 			</div>
 		</div>
 	)
