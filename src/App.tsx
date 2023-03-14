93a87e5677a92e3daeb564dfbfb071509439e8bb
Emmanuel <emmayodayo@gmail.com>
2023-03-14 18:10:41 +0100

    add NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Component imports
+import NavBar from './containers/NavBar/NavBar';
+
 function App() {
-  return (
-    <div className="App">
-      match test
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBar />
+			match test
+		</div>
+	);
 }
 
 export default App;
---
+++ src/containers/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './NavBar.module.css'
+
+const NavBar = () => {
+	return (
+		<div>
+			<div>
+				<span>logo</span>
+				<span>Nav button</span>
+			</div>
+
+			<div>
+				<span>UserIMAGE</span>
+				<span>User name</span>
+			</div>
+		</div>
+	)
+}
+
+export default NavBar;
