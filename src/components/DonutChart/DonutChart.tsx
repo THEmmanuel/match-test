dec059e207ac3577b25244fc2004f7796f3cdf1a
Emmanuel <emmayodayo@gmail.com>
2023-03-15 11:09:33 +0100

    add report data and charts component

---
+++ src/components/DonutChart/DonutChart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './DonutChart.module.css';
+
+const DonutChart = () => {
+	return (
+		<div>
+			Donut Chart
+		</div>
+	)
+}
+
+export default DonutChart
---
+++ src/components/ReportsData/ReportsData.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './ReportsData.module.css';
+
+const ReportsData = () => {
+	return (
+		<div>
+			Reports data
+		</div>
+	)
+}
+
+export default ReportsData
