8539d23b07e1decacc40854e0592b05231629fa4
Emmanuel <emmayodayo@gmail.com>
2023-03-15 11:03:00 +0100

    set up conditional rendering for reports page

--- src/containers/Reports/Reports.tsx
+++ src/containers/Reports/Reports.tsx
@@ -8,19 +8,20 @@ const Reports = (props: any) => {
 	const groupedDataByProject = reportsData.reduce((acc, item) => {
 		const projectId = item.projectId;
 
-		if(!acc[projectId]){
+		if (!acc[projectId]) {
 			acc[projectId] = []
 		}
 
 		acc[projectId].push(item)
 		return acc
 	}, {})
-	console.log(groupedDataByProject, 'by project')
+	console.log(groupedDataByProject, 'by project');
+
 
 	const groupedDataByGateway = reportsData.reduce((acc, item) => {
 		const gatewayId = item.gatewayId;
 
-		if(!acc[gatewayId]){
+		if (!acc[gatewayId]) {
 			acc[gatewayId] = []
 		}
 
@@ -35,50 +36,39 @@ const Reports = (props: any) => {
 	console.log('filtered items', filteredData)
 
 
-
-
-	const groupDataByGateway = () => {
-
+	// Conditional rendering for project, gateway info and charts based on user input
+	
+	
+	if (props.selectedProject === '' && props.selectedGateway === '') {
+		return (
+			<div>
+				{/* <span>Card</span> */}
+				<span>Tablejjj</span>
+			</div>
+		)
+	} else if (props.selectedProject === '' && props.selectedGateway !== '') {
+		return (
+			<div>
+				<span>Table</span>
+				<span>Chart</span>
+			</div>
+		)
+	} else if (props.selectedProject !== '' && props.selectedGateway !== '') {
+		return (
+			<div>
+				<span>Table</span>
+			</div>
+		)
+	} else if (props.selectedProject !== '' && props.selectedGateway === '') {
+		return (
+			<div>
+				<span>Table</span>
+				<span>Chart</span>
+			</div>
+		)
+	} else {
+		return <div>No data selected</div>
 	}
-
-
-	return (
-		<div className={style.ReportsContainer}>
-			<div>
-				<span>{props.selectedProject.name} | {props.selectedGateway.name}</span>
-				<div>
-					<span>project 1</span>
-					<span>total: total here</span>
-				</div>
-
-				<div>
-					<table>
-						<thead>
-							<th>Date</th>
-							<th>Gateway</th>
-							<th>Transaction ID</th>
-							<th>Amount</th>
-						</thead>
-
-						<tbody>
-							<tr>
-								<td>date</td>
-								<td>gateway num</td>
-								<td>id</td>
-								<td>amt</td>
-							</tr>
-						</tbody>
-
-
-					</table>
-				</div>
-			</div>
-
-			<div>
-				<span>Charts</span>
-			</div>
-		</div>
-	)
 }
 
 export default Reports;
