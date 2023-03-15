473d18681e6b7ea60d6eec759cfc1a33b101afe7
Emmanuel <emmayodayo@gmail.com>
2023-03-15 12:05:00 +0100

    add table for reports data

--- src/components/ReportsData/ReportsData.tsx
+++ src/components/ReportsData/ReportsData.tsx
@@ -5,6 +5,15 @@ const ReportsData = (props: any) => {
 
 	console.log(props.data)
 
+	const ProjectCard = (props: any) => {
+		return (
+			<div>
+				<span>{props.projectName}</span>
+				<span>{props.projectAmount}</span>
+			</div>
+		)
+	}
+
 	return (
 		<div>
 			{Object.entries(props.data).map(([key, value]) => {
@@ -13,24 +22,30 @@ const ReportsData = (props: any) => {
 
 				return (
 					<div key={key}>
-						<div>
-							<span>{key}</span>
-							<strong>Total Amount:</strong> {totalAmount}
-						</div>
+						<ProjectCard
+							projectName={key}
+							projectAmount={totalAmount}
+						/>
 
-						<ul>
-							{value.map((item, index) => (
-								<li key={index}>
-									<div>
-										<p>Payment ID: {item.paymentId}</p>
-										<p>Amount: {item.amount}</p>
-										<p>Gateway ID: {item.gatewayId}</p>
-										<p>Modified: {item.modified}</p>
-										{/* <p>Created: {item.created}</p> */}
-									</div>
-								</li>
-							))}
-						</ul>
+						<table>
+							<thead>
+								<th>Date</th>
+								<th>Gateway</th>
+								<th>Transaction ID</th>
+								<th>Amount</th>
+							</thead>
+
+							<tbody>
+								{value.map((item, index) => (
+									<tr>
+										<td>{item.modified}</td>
+										<td>{item.gatewayId}</td>
+										<td>{item.paymentId}</td>
+										<td>{item.amount}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
 					</div>
 				);
 			})}
