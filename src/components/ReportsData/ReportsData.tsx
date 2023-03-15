ced0a773d76e690cdf7bf57fcb347979d0afbadf
Emmanuel <emmayodayo@gmail.com>
2023-03-15 13:54:55 +0100

    add table toggle state and function

--- src/components/ReportsData/ReportsData.tsx
+++ src/components/ReportsData/ReportsData.tsx
@@ -2,31 +2,23 @@ import React, { useState } from 'react';
 import style from './ReportsData.module.css';
 
 const ReportsData = (props: any) => {
-
 	console.log(props.data)
 
+
 	const ProjectCard = (props: any) => {
+		const [showTable, setShowTable] = useState(false)
+		const tableToggleHandler = () => setShowTable(!showTable)
+
 		return (
-			<div className={style.ProjectHeading}>
-				<span className={style.ProjectName}>{props.projectName}</span>
-				<span className={style.ProjectAmount}>Total: {props.projectAmount}USD</span>
-			</div>
-		)
-	}
+			<>
+				<div className={style.ProjectHeading} onClick = {tableToggleHandler}>
+					<span className={style.ProjectName}>{props.projectName}</span>
+					<span className={style.ProjectAmount}>Total: {props.projectAmount}USD</span>
+				</div>
 
-	return (
-		<div>
-			{Object.entries(props.data).map(([key, value]) => {
-				// Calculate the total amount for this key
-				const totalAmount = value.reduce((acc, item) => acc + item.amount, 0);
 
-				return (
-					<div key={key}>
-						<ProjectCard
-							projectName={key}
-							projectAmount={totalAmount}
-						/>
-
+				{
+					showTable ?
 						<table className={style.ReportsTable}>
 							<thead className={style.ReportsTableHeading}>
 								<th className={style.ReportsTableRowFirst}>Date</th>
@@ -36,7 +28,7 @@ const ReportsData = (props: any) => {
 							</thead>
 
 							<tbody>
-								{value.map((item, index) => (
+								{props.value.map((item, index) => (
 									<tr className={style.ReportsTableRow}>
 										<td className={style.ReportsTableRowFirst}>{item.modified}</td>
 										<td>{item.gatewayId}</td>
@@ -46,6 +38,28 @@ const ReportsData = (props: any) => {
 								))}
 							</tbody>
 						</table>
+						: null
+				}
+
+			</>
+		)
+	}
+
+
+
+	return (
+		<div className={style.ReportsWrapper}>
+			{Object.entries(props.data).map(([key, value]) => {
+				// Calculate the total amount for this key
+				const totalAmount = value.reduce((acc, item) => acc + item.amount, 0);
+
+				return (
+					<div key={key}>
+						<ProjectCard
+							projectName={key}
+							projectAmount={totalAmount}
+							value = {value}
+						/>
 					</div>
 				);
 			})}
